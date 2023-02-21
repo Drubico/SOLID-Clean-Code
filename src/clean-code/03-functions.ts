405f78105fdde9328e61e62e5c0b3a2b61f69c9b
Diego Alejandro Rubi <00121117@uca.edu.sv>
2023-02-21 10:00:47 -0600

    17- Cambiando funciones adicionales

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -34,10 +34,10 @@
 	}
 
 	interface Movie {
-		title:          string;
-		description:    string;
-		rating:         number;
-		cast:           string[];
+		title: string;
+		description: string;
+		rating: number;
+		cast: string[];
 	}
 
 	function createMovie({ title, description, rating, cast }: Movie) {
@@ -64,4 +64,40 @@
 		console.log("Crear actor");
 		return true;
 	}
+
+	const getPayAmountBad = ({
+		isDead = false,
+		isSeparated = true,
+		isRetired = false,
+	}) => {
+		let result;
+		if (isDead) {
+			result = 1500;
+		} else {
+			if (isSeparated) {
+				result = 2500;
+			} else {
+				if (isRetired) {
+					result = 3000;
+				} else {
+					result = 4000;
+				}
+			}
+		}
+
+		return result;
+	};
+
+	const getPayAmount = ({
+		isDead = false,
+		isSeparated = true,
+		isRetired = false,
+	}): number => {
+		
+		if (isDead) return 1500;
+
+		if (isSeparated) return 2500;
+
+		return isRetired ? 3000 : 4000;
+	};
 })();
